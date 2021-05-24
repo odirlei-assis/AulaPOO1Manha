4cea1a1f3e953a97ce2e07a585d3fc0127cf1551
Odirlei.Sabella <odirlei@info.local>
2021-05-24 11:12:52 -0300

    Aula POO 24/05

---
+++ Classes/Credito.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace ExemploPOO1.Classes
+{
+    public class Credito : Cartao
+    {
+        private float Limite = 3000f;
+
+        public void Pagar()
+        {
+            float valor = this.Valor;
+            int vezes;
+
+            if (valor > Limite)
+            {
+                Console.WriteLine("Você nao tem limite suficiente para fazer esta compra!");
+            }
+            else
+            {
+
+                do
+                {
+                    Console.WriteLine($"Em quantas vezes deseja parcelar o valor {valor:C2}? (De 1 à 12)");
+                    vezes = int.Parse(Console.ReadLine());
+
+                    if (vezes >= 1 && vezes <= 6)
+                    {
+                        Console.WriteLine($"Você optou por pacelar em {vezes} vezes, dessa forma o juros será de 5% do valor total");
+                        Console.WriteLine($"Você pagará {valor * 1.05:C2} em {vezes} vezes de {(valor * 1.05) / vezes:C2}");
+                    }
+                    else if (vezes >= 7 && vezes <= 12)
+                    {
+                        Console.WriteLine($"Você optou por pacelar em {vezes} vezes, dessa forma o juros será de 8% do valor total");
+                        Console.WriteLine($"Você pagará {valor * 1.08:C2} em {vezes} vezes de {(valor * 1.08) / vezes:C2}");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossivel parcelar nessa quantidade, escolha um numero de vezes entre 1 e 12");
+                    }
+
+                } while (vezes > 12 || vezes < 1);
+            }
+        }
+    }
+}
---
+++ Classes/Debito.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ExemploPOO1.Classes
+{
+    public class Debito : Cartao
+    {
+        private float Saldo = 600f;
+
+        public void Pagar(){
+            float valor = this.Valor;
+
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Você não possui saldo suficiente para pagar esse valor!");
+            } else {
+                Console.WriteLine($"O valor de {valor:C2} foi pago com sucesso!");
+            }
+        }
+    }
+}
